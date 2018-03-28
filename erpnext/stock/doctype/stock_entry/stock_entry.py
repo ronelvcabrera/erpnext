526ec0f713b5d71e71b5367c5d32325ecb1d9294
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2018-03-28 10:25:08 +0300

    Remove Move from Batch Dashboard, Remove Move/Add from Item Dashboard

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -35,9 +35,6 @@ form_grid_templates = {
 }
 
 class StockEntry(StockController):
-	def get_feed(self):
-		return self.stock_entry_type
-
 	def onload(self):
 		for item in self.get("items"):
 			item.update(get_bin_details(item.item_code, item.s_warehouse))
