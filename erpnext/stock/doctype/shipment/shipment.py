eb5a170d13403090173dc61e1993f5ae92c9cef8
Ronel Cabrera <ronelvcabrera007@gmail.com>
2020-07-23 16:58:55 +0800

    feat(Shipment): integrate LetMeShip
    re https://github.com/newmatik/shipment/issues/97

--- erpnext/stock/doctype/shipment/shipment.py
+++ erpnext/stock/doctype/shipment/shipment.py
@@ -12,6 +12,7 @@ from frappe.model.document import Document
 from erpnext.accounts.party import get_party_shipping_address
 from frappe.contacts.doctype.contact.contact import get_default_contact
 from frappe.contacts.doctype.address.address import get_address_display
+from frappe.integrations.doctype.letmeship.letmeship import LETMESHIP_PROVIDER, get_letmeship_available_services, create_letmeship_shipment, get_letmeship_label, get_letmeship_tracking_data
 from frappe.integrations.doctype.packlink.packlink import PACKLINK_PROVIDER, get_packlink_available_services, create_packlink_shipment, get_packlink_label, get_packlink_tracking_data
 from frappe.utils import today
 
@@ -44,19 +45,45 @@ def fetch_shipping_rates(pickup_from_type, delivery_to_type, pickup_address_name
 		Return Shipping Rates for the various Shipping Providers
 	"""
 	shipment_prices = []
-	enabled = frappe.db.get_single_value('Packlink','enabled')
+	letMeShipEnabled = frappe.db.get_single_value('LetMeShip','enabled')
+	packlinkEnabled = frappe.db.get_single_value('Packlink','enabled')
 	pickup_address = get_address(pickup_address_name)
 	delivery_address = get_address(delivery_address_name)
-	if enabled:
+	if letMeShipEnabled:
+		pickup_contact = None
+		delivery_contact = None
+		if pickup_from_type != 'Company':
+			pickup_contact = get_contact(pickup_contact_name)
+		else:
+			pickup_contact = get_company_contact()
+		
+		if delivery_to_type != 'Company':
+			delivery_contact = get_contact(delivery_contact_name)
+		else:
+			delivery_contact = get_company_contact()
+		letmeship_prices = get_letmeship_available_services(
+			delivery_to_type=delivery_to_type,
+			pickup_address=pickup_address,
+			delivery_address=delivery_address,
+			shipment_parcel=shipment_parcel,
+			description_of_content=description_of_content,
+			pickup_date=pickup_date,
+			value_of_goods=value_of_goods,
+			pickup_contact=pickup_contact,
+			delivery_contact=delivery_contact,
+		)
+		letmeship_prices = match_parcel_service_type_carrier(letmeship_prices, ['name', 'carrier'])
+		shipment_prices = shipment_prices + letmeship_prices
+	if packlinkEnabled:
 		packlink_prices = get_packlink_available_services(
 			pickup_address=pickup_address,
 			delivery_address=delivery_address,
 			shipment_parcel=shipment_parcel, 
 			pickup_date=pickup_date
 		)
-		shipment_prices = packlink_prices
-	shipment_prices = sorted(shipment_prices, key=lambda k:
-							 k['total_price'])
+		packlink_prices = match_parcel_service_type_carrier(packlink_prices, ['name', 'carrier_name'])
+		shipment_prices = shipment_prices+ packlink_prices
+	shipment_prices = sorted(shipment_prices, key=lambda k:k['total_price'])
 	return shipment_prices
 
 @frappe.whitelist()
@@ -69,20 +96,35 @@ def create_shipment(shipment, pickup_from_type, delivery_to_type, pickup_address
 	"""
 	service_info = json.loads(service_data)
 	shipment_info = None
+	pickup_contact = None
+	delivery_contact = None
 	pickup_address = get_address(pickup_address_name)
 	delivery_address = get_address(delivery_address_name)
+	if pickup_from_type != 'Company':
+		pickup_contact = get_contact(pickup_contact_name)
+	else:
+		pickup_contact = get_company_contact()
+	
+	if delivery_to_type != 'Company':
+		delivery_contact = get_contact(delivery_contact_name)
+	else:
+		delivery_contact = get_company_contact()
+	if service_info['service_provider'] == LETMESHIP_PROVIDER:
+		shipment_info = create_letmeship_shipment(
+			pickup_address=pickup_address,
+			delivery_address=delivery_address,
+			shipment_parcel=shipment_parcel,
+			description_of_content=description_of_content,
+			pickup_date=pickup_date,
+			value_of_goods=value_of_goods,
+			pickup_contact=pickup_contact,
+			delivery_contact=delivery_contact,
+			service_info=service_info,
+			shipment_notific_email=shipment_notific_email,
+			tracking_notific_email=tracking_notific_email,
+		)
+
 	if service_info['service_provider'] == PACKLINK_PROVIDER:
-		pickup_contact = None
-		if pickup_from_type != 'Company':
-			pickup_contact = get_contact(pickup_contact_name)
-		else:
-			pickup_contact = get_company_contact()
-		
-		if delivery_to_type != 'Company':
-			delivery_contact = get_contact(delivery_contact_name)
-		else:
-			delivery_contact = get_company_contact()
-		
 		shipment_info = create_packlink_shipment(
 			pickup_address=pickup_address,
 			delivery_address=delivery_address,
@@ -107,10 +149,11 @@ def create_shipment(shipment, pickup_from_type, delivery_to_type, pickup_address
 	return shipment_info
 
 
-
 @frappe.whitelist()
 def print_shipping_label(service_provider, shipment_id):
-	if service_provider == PACKLINK_PROVIDER:
+	if service_provider == LETMESHIP_PROVIDER:
+		shipping_label = get_letmeship_label(shipment_id)
+	elif service_provider == PACKLINK_PROVIDER:
 		shipping_label = get_packlink_label(shipment_id)
 	return shipping_label
 
@@ -122,7 +165,9 @@ def update_tracking(shipment, service_provider, shipment_id, delivery_notes=[]):
 	# 	tracking_data = get_letmeship_tracking_data(shipment_id)
 	# else:
 	tracking_data = None
-	if service_provider == PACKLINK_PROVIDER:
+	if service_provider == LETMESHIP_PROVIDER:
+		tracking_data = get_letmeship_tracking_data(shipment_id)
+	elif service_provider == PACKLINK_PROVIDER:
 		tracking_data = get_packlink_tracking_data(shipment_id)
 	if tracking_data:
 		if delivery_notes:
@@ -165,7 +210,6 @@ def update_tracking_info():
 		Daily scheduled event to update Tracking info for not delivered Shipments
 		Also Updates the related Delivery Notes
 	"""
-
 	shipments = frappe.get_all('Shipment', filters={
 		'docstatus': 1,
 		'status': 'Booked',
@@ -196,8 +240,7 @@ def get_address(address_name):
 		'pincode',
 		'country',
 	], as_dict=1)
-	address.country_code = frappe.db.get_value('Country',
-											   address.country, 'code').upper()
+	address.country_code = frappe.db.get_value('Country', address.country, 'code').upper()
 	if not address.pincode or address.pincode == '':
 		frappe.throw(_("Postal Code is mandatory to continue. </br> \
 				Please set Postal Code for Address <a href='#Form/Address/{0}'>{1}</a>"
@@ -249,3 +292,10 @@ def get_company_contact():
 		contact.title = 'MR'
 	contact.email = 'service@eso-hygiene.com'
 	return contact
+
+def match_parcel_service_type_carrier(shipment_prices, reference):
+	for prices in shipment_prices:
+		# service_name = match_parcel_service_type_alias(prices[reference], prices[reference])
+		# is_preferred = frappe.db.get_value('Parcel Service Type', service_name, 'show_in_preferred_services_list')
+	return shipment_prices
+	
